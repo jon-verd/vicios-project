fea6fbc3649cee13c4f2e87fdd9919d7b0252636
Jonathan Verduzco <96271886+jon-verd@users.noreply.github.com>
2022-07-27 03:34:07 -0600

    Getting started with my Create React App for the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Route, Switch } from 'react-router';
+import React from 'react';
+// import {Provider} from "react-redux";
+// import { Home } from './frontend/home/Home';
+// import { FourOhFour } from './fourohfour';
+
+
+
+// export const App = (
+//   <>
+//     <Provider>
+//       <BrowserRouter>
+//         <Switch>
+//           <Route exact path='/' component={Home} />
+//           <Route component={FourOhFour} />
+//         </Switch>
+//       </BrowserRouter>
+//     </Provider>
+//   </>
+// )
+
+
 
 function App() {
   return (

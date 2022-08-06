8dfdb23c2fb4a87671cc83cb21665ba99b768eaf
Jonathan Verduzco <96271886+jon-verd@users.noreply.github.com>
2022-08-06 00:56:45 -0600

    created a section for a clock in the middle of the Navbar. Also made a clock with JS.

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,6 +7,30 @@ import { useState } from "react";
 
 
 
+export function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+
+    if(hh == 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+    }
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss + " " + session;
+
+    document.getElementById("clock").innerText = time; 
+    let t = setTimeout(function(){ currentTime() }, 1000);
+};
 
 export default function MyNavbar() {
     const [navbar, setNavbar] = useState(false);
@@ -56,6 +80,9 @@ export default function MyNavbar() {
                             </button>
                         </div>
                     </div>
+                </div>
+                <div id="clock" className="flex justify-between py-3 md:py-5 -md:invisible">
+                    <currentTime/>
                 </div>
                 <div>
                     <div
